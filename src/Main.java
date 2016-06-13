b37aca7e76fcbf21be78675ba7bffa76d935551e
Lars Grotehenne <Lars@Grotehenne.de>
2016-06-13 21:07:40 +0200

    add some test code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Model m = ModelFactory.createDefaultModel();
+
+        String NS = "http://example.com/test/";
+
+        Resource r = m.createResource (NS + "r");
+        Property p = m.createProperty (NS + "p");
+
+        r.addProperty (p, "hello world", XSDDatatype.XSDstring);
+
+        m.write (System.out, "Turtle");
     }
 }

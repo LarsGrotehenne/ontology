909ab229cbd6433d4d1b64a150b439ea8a5491fa
Lars Grotehenne <Lars@Grotehenne.de>
2016-06-13 22:01:43 +0200

    add test sparql query

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.util.FileManager;
 import org.apache.log4j.LogManager;
@@ -10,13 +11,40 @@ public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
+    // create an empty model
+    public static Model model = ModelFactory.createDefaultModel();
+
     public static void main(String[] args) {
+        System.out.println("Read RDF XML File");
         readRDFXML();
+        System.out.println("---------------------------------------------");
+        System.out.println("Sparql");
+        sparqlQuery();
+    }
+
+    public static void sparqlQuery() {
+        // Create a new query
+        String queryString =
+        "PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#>" +
+        "SELECT ?fullname " +
+        "WHERE {" +
+            "      ?user vcard:FN ?fullname . " +
+            "      }";
+
+        Query query = QueryFactory.create(queryString);
+
+        // Execute the query and obtain results
+        QueryExecution qe = QueryExecutionFactory.create(query, model);
+        ResultSet results = qe.execSelect();
+
+        // Output query results
+        ResultSetFormatter.out(System.out, results, query);
+
+        // Important – free up resources used running the query
+        qe.close();
     }
 
     public static void readRDFXML() {
-        // create an empty model
-        Model model = ModelFactory.createDefaultModel();
 
         // use the FileManager to find the input file
         InputStream in = FileManager.get().open( "test.xml" );

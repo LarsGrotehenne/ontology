70f3da1e10a6281a3bffe3b4a5d0c3c5ad4635a7
Lars Grotehenne <Lars@Grotehenne.de>
2016-06-14 17:18:50 +0200

    add rdfReader

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,8 @@ public class Main {
     public static Model model = ModelFactory.createDefaultModel();
 
     public static void main(String[] args) {
+
+        "rdf/test.xml"
         System.out.println("Read RDF XML File");
         readRDFXML();
         System.out.println("---------------------------------------------");
---
+++ src/rdf/rdfReader.java
@@ -0,0 +1,48 @@
+package rdf;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+import java.io.InputStream;
+
+/**
+ * reads rdf xml or rdf turtle files
+ */
+public class rdfReader {
+
+    /**
+     * reads an RDF XML file
+     * @param modelLocation location of the rdf file
+     * @return model
+     */
+    public Model readRDFXML(String modelLocation) {
+        Model model = ModelFactory.createDefaultModel();
+
+        // use the FileManager to find the input file
+        InputStream in = FileManager.get().open(modelLocation);
+        if (in == null) {
+            throw new IllegalArgumentException(
+                    "File: " + modelLocation + " not found");
+        }
+
+        // read the RDF/XML file
+        model.read(in, null);
+
+        // write it to standard out
+        model.write(System.out);
+
+        return model;
+    }
+
+    /**
+     * reads an RDF Turtle file
+     * @param modelLocation location of the rdf file
+     * @return model
+     */
+    public Model readRDFTurtle(String modelLocation) {
+
+        return null;
+    }
+
+}

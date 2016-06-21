4121e39a2efbc7b384c91c8987f1fbd9f169b282
Lars Grotehenne <Lars@Grotehenne.de>
2016-06-21 11:11:12 +0200

    push A_Scenario

---
+++ src/functionalities/A_Scenario.java
@@ -0,0 +1,30 @@
+package functionalities;
+
+import org.apache.jena.rdf.model.Model;
+
+/**
+ * Selektion von einem oder mehreren Clusteringalgorithmen durch das Angeben
+ * unterschiedlicher Eigenschaften.
+ */
+public class A_Scenario extends FunctionObject {
+    public A_Scenario(Model _model) {
+        super(_model);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Zurzeit keine Functionalitaeten.");
+    }
+
+    @Override
+    public String getDescription() {
+        return "Selektion von einem oder mehreren " +
+                "Clusteringalgorithmen durch das Angeben " +
+                "unterschiedlicher Szenarien.";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "scenario";
+    }
+}

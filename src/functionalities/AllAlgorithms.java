06c3b2d6feb65c54fdd69276a5dfc0b6a3c65426
Lars Grotehenne <Lars@Grotehenne.de>
2016-07-25 23:19:32 +0200

    add GetAllAlgorithms function

---
+++ src/functionalities/AllAlgorithms.java
@@ -0,0 +1,33 @@
+package functionalities;
+
+import helpers.queries;
+import org.apache.jena.rdf.model.Model;
+
+/**
+ * Created by Lars on 25.07.2016.
+ */
+public class AllAlgorithms extends FunctionObject {
+    /**
+     * constructor
+     *
+     * @param _model current model
+     */
+    public AllAlgorithms(Model _model) {
+        super(_model);
+    }
+
+    @Override
+    public void start() {
+        queries.getAllAlgorithms(model);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Gibt alle Algorithmen zurück.";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "algorithms";
+    }
+}

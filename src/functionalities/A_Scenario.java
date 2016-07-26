4481e0f615b2b52292ca543331f0d48d1349ee21
Lars Grotehenne <Lars@Grotehenne.de>
2016-07-26 14:47:51 +0200

    add the scenario functionality A

--- src/functionalities/A_Scenario.java
+++ src/functionalities/A_Scenario.java
@@ -1,5 +1,7 @@
 package functionalities;
 
+import helpers.console;
+import helpers.queries;
 import org.apache.jena.rdf.model.Model;
 
 /**
@@ -13,7 +15,39 @@ public class A_Scenario extends FunctionObject {
 
     @Override
     public void start() {
-        System.out.println("Zurzeit keine Functionalitaeten.");
+
+        queries.getAllClustertypes(model);
+        System.out.println("Einen oder mehrere Clustertypen eingeben, getrennt mit Komma, ohne Leerzeichen, z.B. 'encircled,not_convex'");
+        String input = console.readLine();
+        String types[] = input.split(",");
+        String filter = "";
+        for(String type : types) {
+            filter = filter + " :" + type;
+        }
+            String queryString =
+                    "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+                    "PREFIX : <http://cluster.info#>" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                    "PREFIX  list: <http://jena.hpl.hp.com/ARQ/list#>" +
+                    "SELECT ?Algorithmus " +
+                    "WHERE {" +
+                        "?algo rdfs:label ?Algorithmus." +
+                        "?algo :properties ?proplist." +
+                        "?proplist list:member ?props." +
+                        "FILTER (?props = :can_cluster)." +
+                        "?props :has_values ?list." +
+                        "filter not exists {" +
+                            "values ?value { "+filter+" }" +
+                            "filter not exists {" +
+                                "?list rdf:rest*/rdf:first ?value" +
+                            "}" +
+                        "}" +
+                    "}";
+
+            queries.createQuery(queryString, model);
+
+
     }
 
     @Override

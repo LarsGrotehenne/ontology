89a977d9baf0ffafc45dc4917fd92a708c80116b
Lars Grotehenne <Lars@Grotehenne.de>
2016-07-26 14:20:43 +0200

    formatting

--- src/functionalities/C_Eigenschaften.java
+++ src/functionalities/C_Eigenschaften.java
@@ -31,24 +31,24 @@ public class C_Eigenschaften extends FunctionObject {
             String propValue[] = properties.split("=");
             String queryString =
                     "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
-                            "PREFIX : <http://cluster.info#>" +
-                            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-                            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                            "PREFIX  list: <http://jena.hpl.hp.com/ARQ/list#>" +
-                            "SELECT ?Algorithmus " +
-                            "WHERE {" +
-                            "?algo rdfs:label ?Algorithmus." +
-                            "?algo :properties ?proplist." +
-                            "?proplist list:member ?props." +
-                            "FILTER (?props = :"+propValue[0]+")." +
-                            "?props :has_values ?list." +
-                            "filter not exists {" +
+                    "PREFIX : <http://cluster.info#>" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                    "PREFIX  list: <http://jena.hpl.hp.com/ARQ/list#>" +
+                    "SELECT ?Algorithmus " +
+                    "WHERE {" +
+                        "?algo rdfs:label ?Algorithmus." +
+                        "?algo :properties ?proplist." +
+                        "?proplist list:member ?props." +
+                        "FILTER (?props = :"+propValue[0]+")." +
+                        "?props :has_values ?list." +
+                        "filter not exists {" +
                             "values ?value { :"+propValue[1]+" }" +
                             "filter not exists {" +
-                            "?list rdf:rest*/rdf:first ?value" +
+                                "?list rdf:rest*/rdf:first ?value" +
                             "}" +
-                            "}" +
-                            "}";
+                        "}" +
+                    "}";
 
             queries.createQuery(queryString, model);
         }

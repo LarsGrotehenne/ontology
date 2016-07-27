76f6fca6061f5228f8c2f2bf2d00e3756ab8061d
Lars Grotehenne <Lars@Grotehenne.de>
2016-07-27 22:16:43 +0200

    fix property mode

--- src/functionalities/C_Eigenschaften.java
+++ src/functionalities/C_Eigenschaften.java
@@ -21,60 +21,38 @@ public class C_Eigenschaften extends FunctionObject {
     public void start() {
 
         queries.getAllProperties(model);
-        System.out.println("Es gibt 2 Optionen:");
-        System.out.println("1. Angeben von verschiedenen Eigenschaften, z.B. 'multi_dim,handle_noise,take_parameter'");
-        System.out.println("2. Angeben von einer Eigenschaft mit Wert, z.B. 'speed=normal'");
+        System.out.println("Bitte Eigenschaften und Werte angeben, z.B.: 'speed=normal,has_property=handle_noise'");
 
         String properties = console.readLine();
+        String propertyArray[] = properties.split(",");
 
-        if(properties.contains("=")) {
-            String propValue[] = properties.split("=");
-            String queryString =
-                    "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
-                    "PREFIX : <http://cluster.info#>" +
-                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                    "PREFIX  list: <http://jena.hpl.hp.com/ARQ/list#>" +
-                    "SELECT ?Algorithmus " +
-                    "WHERE {" +
+        String queryString =
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+                        "PREFIX : <http://cluster.info#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX  list: <http://jena.hpl.hp.com/ARQ/list#>" +
+                        "SELECT ?Algorithmus " +
+                        "WHERE {" +
                         "?algo rdfs:label ?Algorithmus." +
-                        "?algo :properties ?props." +
-                        "?props :"+propValue[0]+" ?list." +
-                         "filter not exists {" +
-                            "values ?value { :"+propValue[1]+ " }" +
-                            "filter not exists {" +
-                                "?list rdf:rest*/rdf:first ?value" +
-                            "}" +
-                        "}" +
-                    "}";
+                        "?algo :properties ?props.";
 
-            queries.createQuery(queryString, model);
-        }
 
-        else {
-            String propList[] = properties.split(",");
-            String filter = "";
-            for(String prop : propList) {
-                filter = filter + " :" + prop;
-            }
-                String queryString =
-                    "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
-                    "PREFIX : <http://cluster.info#>" +
-                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                    "SELECT ?Algorithmus " +
-                    "WHERE {" +
-                       "?algo rdfs:label ?Algorithmus." +
-                       "?algo :properties ?list." +
-                       "filter not exists {" +
-                          "values ?value {"+filter+"}" +
-                          "filter not exists {" +
-                            "?list rdf:rest*/rdf:first ?value" +
-                          "}" +
-                       "}" +
-                     "}";
-            queries.createQuery(queryString, model);
+
+        for(int i=0; i<propertyArray.length; i++) {
+            String propValue[] = propertyArray[i].split("=");
+
+            queryString=queryString +
+                    "{?props :"+propValue[0]+" ?list"+i+"." +
+                    "filter not exists {" +
+                    "values ?value { :"+propValue[1]+ " }" +
+                    "filter not exists {" +
+                    "?list"+i+" rdf:rest*/rdf:first ?value" +
+                    "}" +
+                    "}}";
         }
+        queryString=queryString+"}";
+        queries.createQuery(queryString, model);
     }
 
     @Override
